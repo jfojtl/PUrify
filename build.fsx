9a2a7d1b6f665b4e7f2fa1a4178ac2d43dee5cda
Jakub Fojtl <jfojtl@gmail.com>
2013-12-31 13:24:37 +0100

    added running xunit tests by default

--- build.fsx
+++ build.fsx
@@ -3,5 +3,49 @@
 
 open Fake
 
-RestorePackages()
+let testDir = "./tests"
+let buildDir = "./build"
 
+let appReferences = !! "src/**/*.csproj"
+let testReferences = !! "test/**/*.csproj"
+
+Target "Clean" (fun _ ->
+    CleanDirs [testDir; buildDir]
+)
+
+Target "RestorePackages" RestorePackages
+
+Target "Build" (fun _ ->
+    MSBuildRelease buildDir "Build" appReferences
+        |> Log "AppBuild-Output: "
+    
+    MSBuildRelease testDir "Build" testReferences
+        |> Log "AppBuild-Output: "
+)
+
+Target "PrepareForTests" (fun _ ->
+    "xunit.runners"
+        |> RestorePackageId (fun p ->
+            { p with 
+                ExcludeVersion = true; 
+                OutputPath = "./tools" })
+)
+
+Target "Tests" (fun _ ->
+    !! (testDir + "/*.Tests.dll")
+        |> xUnit (fun p -> 
+            {p with 
+                ToolPath = "./tools/xunit.runners/tools/xunit.console.exe";
+                OutputDir = testDir })
+)
+
+Target "Default" DoNothing
+
+"Clean" ==> 
+    "RestorePackages" ==>
+    "Build" ==>
+    "PrepareForTests" ==>
+    "Tests" ==>
+    "Default"
+
+RunTargetOrDefault "Default"
